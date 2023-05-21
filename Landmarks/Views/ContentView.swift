0747076043c6a0a1de25f746f1804115915c8b22
Edgar Vergara <edgarvm13@hotmail.com>
2023-05-20 18:43:22 -0600

    building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Edgar Vergara on 20/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarksList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
